17ede7683f558be4e8fc518342e613c3d4776587
Jesus Marenco <jesusmarencodev@gmail.com>
2021-09-30 15:57:55 -0500

    initial proyect

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native';
+import Calculator from './src/screens/Calculator';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Calculator/>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+
---
+++ src/theme/AppTheme.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    fondo : {
+        flex : 1,
+        backgroundColor : 'black'
+    }
+});
